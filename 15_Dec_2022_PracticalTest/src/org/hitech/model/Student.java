430b475e8320a9873d6c6c664b915a9df0869a9a
Abhishek Bharat Jadhav <abjadhavpatil18@gmail.com>
2022-12-15 21:40:28 +0530

    version-1

---
+++ 15_Dec_2022_PracticalTest/src/org/hitech/model/Student.java
@@ -0,0 +1,74 @@
+package org.hitech.model;
+
+import java.util.Objects;
+
+public class Student {
+	int studentId;
+	String studentName;
+	String qualification;
+
+	
+	public Student(){
+		
+	}
+	
+	Student(int studentId ,String studentName, String Qualification){
+		this.studentId=studentId;
+		this.studentName = studentName;
+		this.qualification = qualification;
+	}
+		
+	
+	public void setStudentId(int studentId) {
+
+		this.studentId = studentId;
+
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+
+	}
+
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+
+	}
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public String getQualification() {
+		return qualification;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", studentName=" + studentName + ", qualification=" + qualification
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(qualification, studentId, studentName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return Objects.equals(qualification, other.qualification) && studentId == other.studentId
+				&& Objects.equals(studentName, other.studentName);
+	}
+
+}
